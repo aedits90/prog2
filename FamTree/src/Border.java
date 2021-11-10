6bafb0f8ab91938cf02f1593638c3dc78a7feace
aedits90 <55504148+aedits90@users.noreply.github.com>
2021-11-10 16:54:28 +0100

    border added

---
+++ FamTree/src/Border.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+public class Border {
+
+    Frame f;
+    Border()
+    {
+        f = new Frame();
+   //    public void drawString(text){String text="";}
+        f.setSize(500, 500);
+        f.setVisible(true);
+    }
+}
---
+++ FamTree/src/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+//public class FileIn{
+   // java.io.FileInputStream fin;
+//}
+public class Main {
+
+    public static void main(String[] args) {
+        new Border();
+        System.out.println("Test");
+
+
+    }
+}
