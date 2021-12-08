eeb62636965ab3d499114a21db15f770a0e2ca35
aedits90 <55504148+aedits90@users.noreply.github.com>
2021-12-08 16:01:39 +0100

    Total Failure

--- FamTree/src/Border.java
+++ FamTree/src/Border.java
@@ -1,13 +1,32 @@
 import javax.swing.*;
 import java.awt.*;
-public class Border {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
-    Frame f;
-    Border()
-    {
-        f = new Frame();
-   //    public void drawString(text){String text="";}
-        f.setSize(500, 500);
-        f.setVisible(true);
+public class Border {
+Border() {
+    Frame fr;
+    fr = new Frame();
+    //    public void drawString(text){String text="";}
+    fr.setSize(500, 500);
+    fr.setVisible(true);
+
+
+    String line = "";
+    String splitBy = ";";
+    try {
+//parsing a CSV file into BufferedReader class constructor
+        BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\User\\Documents\\DEIK-PTI\\Prog2\\FamTree\\src\\prog2-main\\FamTree\\csf.csv"));
+        while ((line = br.readLine()) != null)   //returns a Boolean value
+        {
+            String[] oszl = line.split(splitBy);    // use comma as separator
+            System.out.println("ID=" + oszl[0] + " " + oszl[1] + " " + oszl[2]);
+        }
+    } catch (IOException e) {
+        e.printStackTrace();
     }
+    
+}
+
 }
